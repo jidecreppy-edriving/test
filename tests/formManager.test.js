3bd190240f531f698b0d455dbf2fc65dd0611739
Jide Creppy <jide.creppy@edriving.com>
2020-05-11 11:03:20 +0100

    Added support for ES6

--- tests/formManager.test.js
+++ tests/formManager.test.js
@@ -3,12 +3,6 @@ const AWS = require("aws-sdk");
 const dynamo = new AWS.DynamoDB.DocumentClient();
 const formManager = new FormManager(dynamo);
 
-// This is a very important change
-
-test('adds 1 + 2 to equal 3', () => {
-    expect(1+2).toBe(3);
-});
-
 test('Creating a form returns an id', () => {
     let input = {'name':'amazon'};
     let form = formManager.buildForm(input);
